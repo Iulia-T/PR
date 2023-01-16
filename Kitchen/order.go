534a10c3072697aaa91803cc8dbb4e1935240328
Iulia-T <72933695+Iulia-T@users.noreply.github.com>
2023-01-16 09:36:55 +0200

    Delete Kitchen directory

--- Kitchen/order.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Order struct {
-	Id         int   `json:"id"`
-	Items      []int `json:"items"`
-	Priority   int   `json:"priority"`
-	MaxWait    int   `json:"max-wait"`
-	PickUpTime int   `json:"pick-up-time"`
-}
